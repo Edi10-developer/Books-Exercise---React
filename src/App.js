63a24ff458022b3821a5e746fe3de65feb729dd7
Edi <selimi.edi.1991@gmail.com>
2021-03-10 16:22:51 +0100

    Level 3 - Made better
    Just made againg level 3, changed the component and data.js consumed from App.js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Book from './components/Book';
 
-class App extends React.Component{
+//Import data.json 
+import data from './data.json';
 
-render(){
-    return (
-        <div>
-            <Book/>
-        </div>
-    )};
+class App extends React.Component {
+
+    render() {
+
+        // Create arrays with the titles using MAP() and the 2 variables 
+        const titles   = data.map((book) => book.title);
+        const titleOne = titles[0];
+        const titleTwo = titles[1];
+
+        // Create arrays with the authors using MAP() and the 2 variables
+        const authors   = data.map((book) => book.author);
+        const authorOne = authors[0];
+        const authorTwo = authors[1];
+
+        return (
+            <div>
+                <Book
+                    title={titleOne}
+                    author={authorOne}
+                />
+                <Book
+                    title={titleTwo}
+                    author={authorTwo}
+                />
+            </div>
+        )
+    };
 };
 
 export default App;
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,32 +1,14 @@
 import React from 'react';
-//Import data.json 
-import data from '../data.json';
 
 class Book extends React.Component {
-
+ 
     render() {
-        // Create arrays with the titles
-        const titles = data.map((book) => book.title);
-        // Create arrays with the authors
-        const authors = data.map((book) => book.author);
-
         return (
             <div>
-                <h1>Books</h1>
-                <h2>Titles:</h2>
-                <ul>
-                    {titles.map((title, index) => // From titles array
-                        <li key={index}>{title}</li> // Create a <li> element with the index and the title - p.s. I've tried withouth key={} but is not working, we MUST declare the KEY
-                    )} 
-                </ul> 
-                <h2>Authors:</h2>
-                <ul>
-                    {authors.map((author, index) => // From authors array
-                        <li key={index}>{author}</li> // Create a <li> element with the index and the author
-                    )}
-                </ul>
+                <h1>{this.props.title}</h1>
+                <h3>{this.props.author}</h3>
             </div>
-        );
+        )
     }
 }
 export default Book;
